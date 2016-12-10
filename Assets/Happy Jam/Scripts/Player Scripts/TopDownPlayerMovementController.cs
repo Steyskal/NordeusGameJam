26f87ca41a533f02fa2b558903d5b6bc2f507da4
Marko Alerić <markoall1@gmail.com>
2016-12-10 14:27:05 +0100

    TopDownController extending Agent2D

--- Assets/Happy Jam/Scripts/Player Scripts/TopDownPlayerMovementController.cs
+++ Assets/Happy Jam/Scripts/Player Scripts/TopDownPlayerMovementController.cs
@@ -3,31 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [DisallowMultipleComponent]
-[RequireComponent (typeof(Rigidbody2D))]
-public class TopDownPlayerMovementController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class TopDownPlayerMovementController : AI.Agent2D
 {
-	[Header ("Input Axis Properties")]
-	public string HorizontalAxisName = "Horizontal";
-	public string VerticalAxisName = "Vertical";
+    [Header("Input Axis Properties")]
+    public string HorizontalAxisName = "Horizontal";
+    public string VerticalAxisName = "Vertical";
 
-	[Header ("Movement Properties")]
-	public float MovementVelocity = 5.0f;
+    [Header("Movement Properties")]
+    public float MovementVelocity = 5.0f;
 
-	[Header ("Read-Only")]
-	[SerializeField]
-	private Vector2 _movementDirection;
+    [Header("Read-Only")]
+    [SerializeField]
+    private Vector2 _movementDirection;
 
-	private Rigidbody2D _rigidbody2D;
+    private Rigidbody2D _rigidbody2D;
 
-	void Awake ()
-	{
-		_rigidbody2D = GetComponent<Rigidbody2D> ();
-	}
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    protected override void Start()
+    {
+    }
 
-	void Update ()
-	{
-		_movementDirection = new Vector2 (Input.GetAxisRaw (HorizontalAxisName), Input.GetAxisRaw (VerticalAxisName));
-		_rigidbody2D.velocity = _movementDirection * MovementVelocity;
-	}
-
+    public override void Update()
+    {
+        _movementDirection = new Vector2(Input.GetAxisRaw(HorizontalAxisName), Input.GetAxisRaw(VerticalAxisName));
+        _rigidbody2D.velocity = _movementDirection * MovementVelocity;
+        velocity = _rigidbody2D.velocity;
+    }
+    public override void LateUpdate()
+    {
+    }
 }
